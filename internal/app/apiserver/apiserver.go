f7a757c23a7e79c76c977721e88fcf5fe319de6b
Andrii Puhachenko <puga4enko@gmail.com>
2021-06-13 20:41:18 +0300

    Added config to apiserver struct

--- internal/app/apiserver/apiserver.go
+++ internal/app/apiserver/apiserver.go
@@ -1,11 +1,15 @@
 package apiserver
 
 // APIServer ...
-type APIServer struct{}
+type APIServer struct {
+	config *Config
+}
 
 // New ...
-func New() *APIServer {
-	return &APIServer{}
+func New(newconfig *Config) *APIServer {
+	return &APIServer{
+		config: newconfig,
+	}
 }
 
 // Start ...
