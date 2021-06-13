64f02455ef41785aa650a5463adc2dbbc864eb13
Andrii Puhachenko <puga4enko@gmail.com>
2021-06-13 20:25:01 +0300

    Added apiserver type

---
+++ internal/app/apiserver.go
@@ -0,0 +1,14 @@
+package APIServer
+
+// APIServer ...
+type APIServer struct{}
+
+// New ...
+func New() *APIServer {
+	return &APIServer{}
+}
+
+// Start ...
+func (s *APIServer) Start() error {
+	return nil
+}
